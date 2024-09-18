b94d84dcbcdb9a558b5f7ae3d072aeb9b4ba0fd9
MaxBenDuah <maxwellben728@gmail.com>
2024-09-18 21:47:33 +0100

    Implement time and date feature

---
+++ src/ui/TimeAndDate.jsx
@@ -0,0 +1,31 @@
+import { Calendar } from "@phosphor-icons/react";
+
+function TimeAndDate() {
+  const dateNow = new Date();
+
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: "Europe/Berlin",
+    timeZoneName: "short",
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const currentDateAndTime = new Intl.DateTimeFormat("de-De", options).format(
+    dateNow
+  );
+  return (
+    <div className="flex align-items-baseline py-2 border-round-3xl transition-all transition-duration-300 transition-ease-out shadow-2 bg-white justify-content-center">
+      <div>
+        <p className="text-gray-600">Current time</p>
+        <p className="font-bold">{currentDateAndTime}</p>
+      </div>
+      <Calendar size={32} className="pt-2" />
+    </div>
+  );
+}
+
+export default TimeAndDate;
