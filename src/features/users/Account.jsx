3b32cf50a488a08d0ad7993d44abc75ff23cfc3a
MaxBenDuah <maxwellben728@gmail.com>
2024-09-18 21:20:04 +0100

    Implement account feature

---
+++ src/features/users/Account.jsx
@@ -0,0 +1,40 @@
+import { Divider } from "primereact/divider";
+import UpdateUserDataForm from "./updateUserDataForm";
+import UpdateUserPassword from "./UpdateUserPassword";
+import { useUser } from "./useUser";
+import { Button } from "primereact/button";
+import { useMoveBack } from "../../hooks/useMoveBack";
+
+function Account() {
+  const { user } = useUser();
+  const {
+    user_metadata: { name, email },
+  } = user;
+  const goBack = useMoveBack();
+
+  const userData = { name, email };
+  return (
+    <div className="pt-4">
+      <div
+        className="mx-auto shadow-2 p-5 border-round-md"
+        style={{ maxWidth: "640px" }}
+      >
+        <Button
+          icon="pi pi-arrow-left"
+          rounded
+          outlined
+          aria-label="Go back"
+          onClick={goBack}
+        />
+        <h1>Update User Account</h1>
+        <UpdateUserDataForm userData={userData} />
+        <Divider />
+        <h1>Update Password</h1>
+        <p>Please enter your new password</p>
+        <UpdateUserPassword />
+      </div>
+    </div>
+  );
+}
+
+export default Account;
