4fe9a5a60ecaea9fdd3f8b48507cb2f7ee050b7f
MaxBenDuah <maxwellben728@gmail.com>
2024-09-18 21:42:54 +0100

    Implement Loader feature

---
+++ src/ui/Loader.jsx
@@ -0,0 +1,32 @@
+function Loader() {
+  return (
+    <div className="overlay">
+      <div className="section-center">
+        <div className="section-path">
+          <div className="globe">
+            <div className="wrapper">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
