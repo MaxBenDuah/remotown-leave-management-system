cb3863e85e45ea99cd4054bdb9dbc84dd03c2826
MaxBenDuah <maxwellben728@gmail.com>
2024-09-18 21:23:09 +0100

    Implement reset password

---
+++ src/features/users/ResetPassword.jsx
@@ -0,0 +1,13 @@
+import UpdateUserPassword from "./UpdateUserPassword";
+
+function ResetPassword() {
+  return (
+    <div>
+      <h1>Update Password</h1>
+      <p>Please enter your new password</p>
+      <UpdateUserPassword />
+    </div>
+  );
+}
+
+export default ResetPassword;
