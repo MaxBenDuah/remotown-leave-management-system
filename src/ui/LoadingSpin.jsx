ee8b22d0e4c554f2262680f74437fce46a5f98e8
MaxBenDuah <maxwellben728@gmail.com>
2024-09-18 21:43:32 +0100

    Implement loading spinner feature

---
+++ src/ui/LoadingSpin.jsx
@@ -0,0 +1,10 @@
+function LoadingSpin() {
+  return (
+    <div className="loader">
+      <div className="quater i"></div>
+      <div className="quater ii"></div>
+    </div>
+  );
+}
+
+export default LoadingSpin;
