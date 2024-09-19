e45212d70e0ec943899a723da3089944be830408
MaxBenDuah <maxwellben728@gmail.com>
2024-09-19 19:57:27 +0100

    Delete comments in Notifications.js

--- src/features/employees/Notifications.jsx
+++ src/features/employees/Notifications.jsx
@@ -1,48 +1,13 @@
-// import { Dialog } from "primereact/dialog";
-// import { Button } from "primereact/button";
 import { Sidebar } from "primereact/sidebar";
 import { Divider } from "primereact/divider";
-import NotificationItem from "./NotificationItem";
 import { useGetNotifications } from "./useGetNotifications";
-// import { useState } from "react";
+
+import NotificationItem from "./NotificationItem";
 
 function Notifications({ visible, setVisible }) {
-  // const [visible, setVisible] = useState(false);
-  // const [position, setPosition] = useState("center");
-  const {
-    data: notifications,
-    // isLoading,
-    isError,
-    error,
-  } = useGetNotifications();
-
-  // const footerContent = (
-  //   <div>
-  //     <Button
-  //       label="Cancel"
-  //       icon="pi pi-times"
-  //       onClick={() => setVisible(false)}
-  //       className="p-button-text"
-  //     />
-  //     <Button
-  //       label="OK"
-  //       icon="pi pi-check"
-  //       onClick={() => setVisible(false)}
-  //       autoFocus
-  //     />
-  //   </div>
-  // );
-
-  // function show(position) {
-  //   setPosition(position);
-  //   setVisible(true);
-  // }
-
-  // if (isLoading) return <p>Loading notifications...</p>;
+  const { data: notifications, isError, error } = useGetNotifications();
 
   if (isError) return <p>{error.message}</p>;
-
-  // console.log(notifications);
 
   return (
     <div>
@@ -54,13 +19,6 @@ function Notifications({ visible, setVisible }) {
       >
         <h2 className="mb-0 mb-2">Notifications</h2>
         <Divider type="solid" />
-        {/* <div className="border-300 border-bottom-1"></div> */}
-        {/* <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p> */}
         <ul className="list-none p-0">
           {notifications?.map((notification) => (
             <NotificationItem
@@ -70,39 +28,6 @@ function Notifications({ visible, setVisible }) {
           ))}
         </ul>
       </Sidebar>
-      {/* <h2>Notifications</h2> */}
-      {/* <Dialog
-        header="Notifications"
-        visible={visible}
-        position={position}
-        style={{ width: "50vw" }}
-        onHide={() => {
-          if (!visible) return;
-          setVisible(false);
-        }}
-        footer={footerContent}
-        draggable={true}
-        resizable={true}
-      >
-      // This is not part. This is from PrimeReact
-        <p className="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <ul className="list-none">
-          {notifications?.map((notification) => (
-            <NotificationItem
-              key={notification.id}
-              notification={notification}
-            />
-          ))}
-        </ul>
-      </Dialog> */}
     </div>
   );
 }
