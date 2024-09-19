f02f368854b62024d973681e439306f23a6f3ac2
MaxBenDuah <maxwellben728@gmail.com>
2024-09-19 20:03:31 +0100

    Remove comments in custom hooks in employees folder

--- src/features/employees/useGetHolidays.js
+++ src/features/employees/useGetHolidays.js
@@ -1,19 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { getHolidays } from "../../services/apiHolidays";
-// import { useSearchParams } from "react-router-dom";
 
 export function useGetHolidays() {
-  // Since the company is in Germany, I think it would be best to get the holidays for only Germany. Later, I will implement a way to be able to get it for any country
-  // const [searchParams] = useSearchParams();
-
-  // const country = searchParams.get("country");
-  // const year = searchParams.get("year");
-  // const holidayObj = { country, year };
-
   const { data, isLoading, isError, error } = useQuery({
-    // queryKey: ["holidays", holidayObj], - previous
     queryKey: ["holidays"],
-    // queryFn: () => getHolidays(holidayObj), - previous
     queryFn: getHolidays,
   });
 
