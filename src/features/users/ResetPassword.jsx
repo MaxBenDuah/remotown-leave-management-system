31e41f81082eca24528326d390b9df98580c5719
MaxBenDuah <maxwellben728@gmail.com>
2024-09-22 10:41:19 +0100

    Adjust font size, border radius and place ResetPassword component in center of page

--- src/features/users/ResetPassword.jsx
+++ src/features/users/ResetPassword.jsx
@@ -2,10 +2,16 @@ import UpdateUserPassword from "./UpdateUserPassword";
 
 function ResetPassword() {
   return (
-    <div>
-      <h1>Update Password</h1>
-      <p>Please enter your new password</p>
-      <UpdateUserPassword />
+    <div className="h-screen flex align-items-center">
+      <div className="max-w-30rem mx-auto p-3  border-round-lg shadow-2">
+        <h1 className="text-center text-xl md:text-left md:text-4xl">
+          Update Password
+        </h1>
+        <p className="text-center md:text-left">
+          Please enter your new password
+        </p>
+        <UpdateUserPassword />
+      </div>
     </div>
   );
 }
